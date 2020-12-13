33af5d92f3db9e2f931ec088c6b364ea79b83ee6
fabrica-soft-mty <gago.gmz18@gmail.com>
2020-12-12 22:08:37 -0600

    Version 2

--- index.js
+++ index.js
@@ -70,11 +70,9 @@ app.use((req, res, next) => {
 
 app.use('/', routes());
 
-//purto en el que arranca el servidor de desarrollo
-// app.listen(4000);
 // Puerto y host para la app
 const host = process.env.HOST || '0.0.0.0';
 const port = process.env.PORT || '3000';
 app.listen(port, host, () => {
-    console.log('El servidor esta funcionando')
+    console.log(`El servidor esta funcionando puerto ${port}`);
 });
